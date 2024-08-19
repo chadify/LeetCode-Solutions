3e41423b9ae9b505eacbbbf0ca3181a738359475
chadify <72332949+chadify@users.noreply.github.com>
2024-08-19 19:21:30 +0530

    0069-SQRTX Stats: Time: 1 ms (85.69%), Space: 40.7 MB (50.08%) - LeetHub

--- 0069-sqrtx/0069-sqrtx.java
+++ 0069-sqrtx/0069-sqrtx.java
@@ -1,8 +1,23 @@
 class Solution {
     public int mySqrt(int x) {
-        long i;
-        for (i = 0; i*i <= x; i++)
-            ;
-        return (int)i - 1;
+
+        if(x == 0 || x == 1)
+            return x;
+
+        int start = 1;
+        int end = x;
+        int mid = -1; 
+
+        while (start <= end) {
+            mid = start + (end - start)/2;
+            if ((long)mid*mid > (long)x)
+                end = mid - 1;
+            else if ((long)mid*mid == (long)x)
+                return mid;
+            else
+                start = mid + 1;
+        }
+
+        return Math.round(end);
     }
 }
