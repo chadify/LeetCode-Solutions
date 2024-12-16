aaeb2b90970baf97e9c2abd3f51fbab94fac14d2
chadify <72332949+chadify@users.noreply.github.com>
2024-12-16 15:10:08 +0530

    Time: 1 ms (50.02%), Space: 44.6 MB (83.33%) - LeetHub

--- 0941-sort-array-by-parity/0941-sort-array-by-parity.java
+++ 0941-sort-array-by-parity/0941-sort-array-by-parity.java
@@ -1,25 +1,17 @@
 class Solution {
     public int[] sortArrayByParity(int[] nums) {
-        int i = 0, j = 0; // i tracks the current position, j tracks the position of next even number
+        int left = 0, right = nums.length - 1;
         
-        while (i < nums.length) {
-            // Find the next odd number
-            while (i < nums.length && nums[i] % 2 == 0) 
-                i++;
-            
-            // Find the next even number after j
-            j = Math.max(j, i);
-            while (j < nums.length && nums[j] % 2 != 0) 
-                j++;
-            
-            // Swap if both pointers are in bounds
-            if (j < nums.length && i < nums.length) {
-                int temp = nums[i];
-                nums[i] = nums[j];
-                nums[j] = temp;
+        while (left < right) {
+            if (nums[left] % 2 > nums[right] % 2) {
+                // Swap odd on the left with even on the right
+                int temp = nums[left];
+                nums[left] = nums[right];
+                nums[right] = temp;
             }
             
-            i++; // Move to the next position
+            if (nums[left] % 2 == 0) left++;
+            if (nums[right] % 2 != 0) right--;
         }
         
         return nums;
