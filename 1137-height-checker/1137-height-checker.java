f9333b14b361781e7066c745298ccf8870f4e0e2
chadify <72332949+chadify@users.noreply.github.com>
2024-12-17 19:17:59 +0530

    Time: 0 ms (100%), Space: 42.2 MB (8.47%) - LeetHub

--- 1137-height-checker/1137-height-checker.java
+++ 1137-height-checker/1137-height-checker.java
@@ -1,7 +1,6 @@
 class Solution {
     public int heightChecker(int[] heights) {
-        int[] expected = heights.clone();
-        Arrays.sort(expected);
+        int[] expected = mergeSort(heights, 0, heights.length - 1);
 
         int res = 0;
         for (int i = 0; i < heights.length; i++) {
@@ -10,6 +9,32 @@ class Solution {
         }
 
         return res;
-        
+    }
+
+    private int[] mergeSort(int[] array, int s, int e) {
+        if (s == e) { // Base case: single element
+            return new int[]{array[s]};
+        }
+        int m = s + (e - s) / 2;
+        int[] left = mergeSort(array, s, m);
+        int[] right = mergeSort(array, m + 1, e);
+
+        return merge(left, right);
+    }
+
+    private int[] merge(int[] left, int[] right) {
+        int[] res = new int[left.length + right.length];
+        int i = 0, j = 0, k = 0;
+
+        while (i < left.length && j < right.length) {
+            if (left[i] < right[j])
+                res[k++] = left[i++];
+            else
+                res[k++] = right[j++];
+        }
+
+        while (i < left.length) res[k++] = left[i++];
+        while (j < right.length) res[k++] = right[j++];
+        return res;
     }
 }
