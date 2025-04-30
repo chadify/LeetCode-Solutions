e954f259284fc5777fb58bae5c4931e1d5617d90
chadify <72332949+chadify@users.noreply.github.com>
2025-05-01 01:13:10 +0530

    0792-BINARY-SEARCH Stats: Time: 0 ms (100%), Space: 46.1 MB (22.91%) - LeetHub

--- 0792-binary-search/0792-binary-search.java
+++ 0792-binary-search/0792-binary-search.java
@@ -1,18 +1,20 @@
 class Solution {
     public int search(int[] nums, int target) {
-        return binarySearch(nums, target, 0, nums.length - 1);
-    }
+        int left = 0, right = nums.length - 1;
 
-    private static int binarySearch(int[] arr, int target, int start, int end) {
-        if (start > end)
-            return -1;
+        while (left <= right) {
+            int mid = left + (right - left)/2;
+            if (nums[mid] < target) {
+                left = mid + 1;
+            }
+            else if (nums[mid] == target) {
+                return mid;
+            }
+            else {
+                right = mid - 1;
+            }
+        }
 
-        int mid = start + (end - start)/2;
-        if (arr[mid] < target)
-            return binarySearch(arr, target, mid+1, end);
-        else if (arr[mid] == target)
-            return mid;
-        else
-            return binarySearch(arr, target, start, mid-1);
+        return -1;
     }
 }
